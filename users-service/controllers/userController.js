0ea4bf610db8aca7a28c8acd3fdaab1bbef15e36
UEBERO <contactoyaser@gmail.com>
2024-10-17 06:27:36 +0200

    Updated JWT token on login and register

--- users-service/controllers/userController.js
+++ users-service/controllers/userController.js
@@ -1,13 +1,23 @@
 // controllers/userController.js
 const User = require('../models/User');
 const { getOrdersByUserId } = require('../services/orderService');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // Crear un nuevo usuario
 exports.createUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    const user = new User({ name, email, password });
+    // Password hashing
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    // Create and save user
+    const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+    // JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.status(201).send(user);
   } catch (err) {
     res.status(400).send(err);
@@ -18,9 +28,14 @@ exports.createUser = async (req, res) => {
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findByCredentials(email, password);
-    const token = await user.generateAuthToken();
-    res.status(200).send({ user, token });
+    const user = await User.findOne({email});
+    if(!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(401).send('Email o contraseña incorrectos');
+    }
+
+    // JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).send({ auth:true , token });
   } catch (err) {
     res.status(400).send(err);
   }
@@ -53,7 +68,15 @@ exports.getUserById = async (req, res) => {
 // Actualizar un usuario por ID
 exports.updateUserById = async (req, res) => {
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    const updateData = { ...req.body };
+
+    // Si la contraseña está presente en los datos de actualización, hashearla
+    if (updateData.password) {
+      const salt = bcrypt.genSaltSync(10);
+      updateData.password = bcrypt.hashSync(updateData.password, salt);
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.id, updateData, { new: true });
     if (!user) {
       return res.status(404).send('Usuario no encontrado');
     }
@@ -68,9 +91,9 @@ exports.deleteUserById = async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
     if (!user) {
-      return res.status(404).send('Usuario no encontrado');
+      return res.status(404).send({error:'Usuario no encontrado'});
     }
-    res.status(200).send('Usuario eliminado');
+    res.status(200).send({message:'Usuario eliminado correctamente'});
   } catch (err) {
     res.status(400).send(err);
   }
